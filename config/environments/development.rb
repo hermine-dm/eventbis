d8a672e81155e22929ca96aaa4a496e1b7800477
hermine-dm <herdementh@gmail.com>
2019-11-04 13:43:48 +0100

    version 2activemailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true #false permet de stopper tout envoi de mail
 end
